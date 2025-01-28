f147c04c5d117fc3e5b1325522fcc9dbd2c1855b
Hardik Prajapati <135410619+HardikPrajapati555@users.noreply.github.com>
2025-01-28 14:43:13 +0530

    Update Register.js

--- src/components/Register.js
+++ src/components/Register.js
@@ -17,7 +17,7 @@ const Register = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post("http://localhost:5000/api/users/register", formData);
+      const response = await axios.post("https://backend-api-a4xs.onrender.com/api/users/register", formData);
       alert("Registration successful!");
       console.log(response.data);
     } catch (error) {
